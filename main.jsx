4e818a4fce730278709744ccd76594c66e1f9272
kartikbala908 <kartikbalapoko@gmail.com>
2024-07-20 09:22:23 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css'
+import App4 from './component/App4.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App4/>
+  </React.StrictMode>,
+)
